bbd3361bba8eb240bd3a245e142e2ec277c010d2
Dan DeLeo <danielsdeleo@mac.com>
2009-09-08 22:56:44 -0600

    rm pending spec as I'm being to lazy to get to it ;)

--- spec/unit/moqueue_spec.rb
+++ spec/unit/moqueue_spec.rb
@@ -2,8 +2,4 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Moqueue do
   
-  it "should have mocha-like #expects_message('message content') expectations" do
-    pending("after refactoring: I'm ugly on the inside. like Hollywood")
-  end
-  
 end
