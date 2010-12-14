8d9bd8feeb21165f673f4dffcc04340c74b6d332
Christopher R. Murphy <chmurph2+git@gmail.com>
2010-12-14 17:40:18 -0500

    Removed diff cruft.
    http://blogobaggins.com/2009/03/31/waging-war-on-whitespace.html

--- lib/moqueue/overloads.rb
+++ lib/moqueue/overloads.rb
@@ -1,22 +1,22 @@
 require "eventmachine"
 
 class MQ
-  
+
   class << self
     def queue(name)
       Moqueue::MockQueue.new(name)
     end
-    
+
     def direct(name, opts={})
       Moqueue::MockExchange.new(opts.merge(:direct=>name))
     end
-    
+
     def fanout(name, opts={})
       Moqueue::MockExchange.new(opts.merge(:fanout=>name))
     end
 
   end
-  
+
   def initialize(*args)
   end
 
@@ -27,34 +27,34 @@ class MQ
   def fanout(name, opts = {})
     Moqueue::MockExchange.new(opts.merge(:fanout => name))
   end
-  
+
   def queue(name, opts = {})
     Moqueue::MockQueue.new(name)
   end
-  
+
   def topic(topic_name)
     Moqueue::MockExchange.new(:topic=>topic_name)
   end
-  
+
 end
 
 module AMQP
-  
+
   class << self
     attr_reader :closing
     alias :closing? :closing
   end
-  
+
   def self.start(opts={},&block)
     EM.run(&block)
   end
-  
+
   def self.stop
     @closing = true
     yield if block_given?
     @closing = false
   end
-  
+
   def self.connect(*args)
   end
 end
--- spec/unit/overloads_spec.rb
+++ spec/unit/overloads_spec.rb
@@ -1,11 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "AMQP and MQ", "when overloaded by moqueue/overloads" do
-  
+
   before(:all) do
     overload_amqp
   end
-  
+
   it "should make AMQP.start take options and a block without connecting to AMQP broker" do
     ensure_deferred_block_called
     AMQP.start(:host => "localhost") do
@@ -13,44 +13,44 @@ describe "AMQP and MQ", "when overloaded by moqueue/overloads" do
       EM.stop
     end
   end
-  
+
   it "should run EM in AMQP.start" do
     EM.expects(:run)
     AMQP.start { EM.stop }
   end
-  
+
   it "should provide a MQ.queue class method" do
     MQ.queue('FTW').should be_a(Moqueue::MockQueue)
   end
-  
+
   it "should emulate the behavior of MQ.closing?" do
     ensure_deferred_block_called
-    AMQP.stop do 
+    AMQP.stop do
       deferred_block_called
       AMQP.should be_closing
     end
   end
-  
+
   it "should create direct exchanges" do
     MQ.new.direct("directamundo").should == MockExchange.new(:direct => "directamundo")
   end
-  
+
   it "should create topic exchanges" do
     MQ.new.topic("lolzFTW").should == MockExchange.new(:topic => "lolzFTW")
   end
-  
+
   it "should provide a MQ.direct class method" do
     MQ.direct("direct", :durable=>true).should be_a(Moqueue::MockExchange)
   end
-  
+
   it "should provide a MQ.fanout class method" do
     MQ.fanout("fanout", :durable=>true).should be_a(Moqueue::MockExchange)
   end
-  
+
   it "should create a named fanout queue via MQ.fanout" do
     fanout = MQ.fanout("SayMyNameSayMyName", :durable=>true)
     fanout.should be_a(Moqueue::MockExchange)
     fanout.fanout.should == "SayMyNameSayMyName"
   end
-  
+
 end
